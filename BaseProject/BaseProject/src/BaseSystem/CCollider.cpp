2fe69fc0ef4e8e79ce40098d3f92617f6c159f34
Namizashi-Shota-Fukuyama <shota-namizashi@anabuki-net.ne.jp>
2025-11-18 11:39:40 +0900

    三角形とレイの当たり判定も同様に距離を設定するよう対応

--- BaseProject/BaseProject/src/BaseSystem/CCollider.cpp
+++ BaseProject/BaseProject/src/BaseSystem/CCollider.cpp
@@ -377,8 +377,9 @@ bool CCollider::CollisionTriangleRay(
 		return false;
 	}
 
-	//ヒット情報に交点を設定
+	//ヒット情報に交点と距離を設定
 	h->cross = cross;
+	h->dist = (cross - rs).Length();
 
 	//調整値計算（衝突しない位置まで戻す）
 	if (dots < 0.0f) {
